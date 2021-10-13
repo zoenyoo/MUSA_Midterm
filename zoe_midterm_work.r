e0363badbaf443764379303b48d79313ea551676
zoenyoo <zoeyoo@berkeley.edu>
2021-10-13 19:23:57 -0400

    Update zoe_midterm_work.r

--- zoe_midterm_work.r
+++ zoe_midterm_work.r
@@ -29,25 +29,46 @@ county_boundary <-
   st_read("https://opendata.arcgis.com/datasets/964b8f3b3dbe401bb28d49ac93d29dc4_0.geojson") %>%
   st_transform(st_crs(boulder.sf))
 
-ggplot() +
-  geom_sf(data=boulder.boundary)+
-  geom_sf(data=playgrounds.sf, color="green")+
-  geom_sf(data=schools$osm_points)+
-  geom_sf(data = boulder.sf, aes(colour = q5(price)), 
-          show.legend = "point", size = 2)
+tracts <-
+  st_read("https://raw.githubusercontent.com/zoenyoo/MUSA_Midterm/main/Modeling_Data/SHP%20data/BoulderCounty_tracts.geojson") %>% 
+  st_transform(st_crs(boulder.sf))
+
+boulder.sf <- st_join(boulder.sf, tracts) %>% 
+  select(c(1:56))
+
+floodplain <-
+  st_read("https://opendata.arcgis.com/datasets/f2b0b0d290894d3faafc6a5c7964445a_0.geojson") %>% 
+  st_transform(st_crs(boulder.sf))
 
 playgrounds.sf <- 
   st_read("https://opendata.arcgis.com/datasets/b89ea27bc3cd492682503f03df1a9fb9_0.geojson") %>%
   st_as_sf(coords=playgrounds.sf$geometry, crs = 4326) %>% 
   st_transform(st_crs(boulder.sf)) 
 
+
 schools <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% 
                              opq() %>% 
                              add_osm_feature("amenity", "school"))
 schools <- schools$osm_points
-
 schoolPoints <- st_as_sf(schools,coords=schools$geometry, crs = 4326) %>% 
   st_transform(st_crs(boulder.sf)) 
+
+water <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% 
+                        opq() %>% 
+                        add_osm_feature("natural", "water")) 
+water <- water$osm_polygons 
+water.sf <- st_as_sf(water,coords=water$geometry, crs = 4326) %>% 
+  st_transform(st_crs(boulder.sf)) %>% 
+  drop_na(name)
+
+ggplot() +
+  geom_sf(data=county_boundary)+
+  geom_sf(data=tracts, alpha=.4)+
+  geom_sf(data=playgrounds.sf, color="green")+
+  geom_sf(data=water.sf)+
+  geom_sf(data = boulder.sf, aes(colour = q5(price)), 
+          show.legend = "point", size = 2)+
+  mapTheme()
 
 boulder.sf$school.Buffer =
   st_buffer(boulder.sf, 660) %>% 
@@ -56,6 +77,6 @@ boulder.sf$school.Buffer =
 
 x <- lengths(st_intersects(st_buffer(boulder.sf, 660), schoolPoints))
 mutate(boulder.sf, school.Buffer=x)
-poly$pt_count <- lengths(st_intersects(poly, pts))
+poly$pt_count <- length(st_intersects(poly, pts))
 pointsBuffer$city_count <- count$name
 
