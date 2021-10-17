10f669cd9f8762e7c33d0c97e7b20cf811d2080f
zoenyoo <zoeyoo@berkeley.edu>
2021-10-17 12:07:43 -0400

    Update zoe_midterm_work.r

--- zoe_midterm_work.r
+++ zoe_midterm_work.r
@@ -15,15 +15,17 @@ library(knitr)
 library(tidycensus)
 library(scales)
 
-root.dir = "C:/Users/zoeny/OneDrive - PennO365/GrSchool/Year1/MUSA 508 Public Policy Analytics/MUSA508/MUSA_Midterm"
+root.dir = "https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-Landing/master/DATA/"
 source("https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-Landing/master/functions.r")
 palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
+palette_rank5 <- c("#ffffb2", "#fecc5c", "#fd8d3c",   "#f03b20", "#bd0026")
 
 boulder.sf <- 
-  st_read(file.path(root.dir,"/studentData.geojson"), ) %>% 
+  st_read("studentData.geojson", ) %>% 
   st_set_crs('ESRI:102254') %>% st_make_valid() %>% 
-  mutate(Age = 2021 - builtYear) %>% 
-  filter(price < 10000000)
+  mutate(Age = 2021 - builtYear,
+         PricePerSF = price/TotalFinishedSF) 
+boulder.sf[2638,]$price <- 315000 #mistake input as 31500000, checked on zillow
 
 county_boundary <- 
   st_read("https://opendata.arcgis.com/datasets/964b8f3b3dbe401bb28d49ac93d29dc4_0.geojson") %>%
@@ -42,56 +44,66 @@ playgrounds.sf <-
   st_as_sf(coords=playgrounds.sf$geometry, crs = 4326) %>% 
   st_transform(st_crs(boulder.sf)) 
 
-parcels.sf <- 
+parcels <- 
   st_read("https://opendata.arcgis.com/datasets/89ae49d4ddf246388ee5f5e952aa84db_0.geojson") %>% 
   st_transform(st_crs(boulder.sf))
 
-boulder.sf <- st_join(boulder.sf, tracts) %>% 
-  select(c(1:56)) 
+boulder.sf <- st_join(boulder.sf, tracts) 
+boulder.sf <- st_join(boulder.sf, parcels) %>% 
+  select(!c(51:54, 56:58, 61:65, 67:60)) 
 
-available_features() #amenity, shop, natural, highway, building
+#available_features() #amenity, shop, natural, highway, building
+
 
 schools <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% 
-                             opq() %>% 
-                             add_osm_feature("amenity", "school"))
-schools <- schools$osm_points
-schoolPoints.sf <- st_as_sf(schools,coords=schools$geometry, crs = 4326) %>% 
-  st_transform(st_crs(boulder.sf)) 
+                        opq() %>% 
+                        add_osm_feature("amenity", "school"))$osm_points
+schools.sf <- st_as_sf(schools, coords=schools$geometry, crs = 4326) %>% 
+  st_geometry(schools.sf$geometry) %>%
+  st_transform(st_crs(boulder.sf))  %>%
+  st_sf() %>%
+  cbind(., schools$name) %>%
+  rename(NAME = schools.name)
+
+parks <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% opq() %>%
+                      add_osm_feature(key = 'leisure', value = "park"))
+parks.sf <- st_geometry(parks$osm_polygons) %>%
+  st_transform(st_crs(boulder.sf)) %>%
+  st_sf() %>%
+  cbind(., parks$osm_polygons$name) %>%
+  rename(NAME = parks.osm_polygons.name)
 
 water <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% 
                         opq() %>% 
-                        add_osm_feature("natural", "water")) 
-water <- water$osm_polygons 
+                        add_osm_feature("natural", "water"))$osm_polygons
 water.sf <- st_as_sf(water,coords=water$geometry, crs = 4326) %>% 
   st_transform(st_crs(boulder.sf)) %>% 
   drop_na(name)
 
 highways <- osmdata_sf(getbb("Boulder County", base_url="https://nominatim.openstreetmap.org") %>% 
                       opq() %>% 
-                      add_osm_feature("highway")) 
-highways <- highways$osm_lines
+                      add_osm_feature("highway"))$osm_lines
 highways.sf <- st_as_sf(highways,coords=highways$geometry, crs = 4326) %>% 
   st_transform(st_crs(boulder.sf)) %>% 
   drop_na(name)
 
+trailheads.sf <-
+  st_read("https://opendata.arcgis.com/datasets/3a950053bbef46c6a3c2abe3aceee3de_0.geojson") %>%
+  st_transform(st_crs(boulder.sf))
+
 ggplot() +
-  geom_sf(data=county_boundary)+
+  geom_sf(data=county_boundary, color="white")+
   geom_sf(data=tracts, alpha=.4)+
-  geom_sf(data=playgrounds.sf, color="green")+
-  geom_sf(data=highways.sf, color="orange")+
-  geom_sf(data = boulder.sf, aes(colour = q5(price)), 
-          show.legend = "point", size = 2)+
+  geom_sf(data = boulder.sf, aes(color = q5(price)), 
+          show.legend = "point", size = 0.3)+
+  scale_color_brewer(type=seq, palette = "YlOrRd")+
   mapTheme()
 
 boulder.sf <- 
   mutate(boulder.sf, water.Buffer=lengths(st_intersects(st_buffer(boulder.sf, 660), water.sf))) %>% 
   mutate(boulder.sf, playground.Buffer=lengths(st_intersects(st_buffer(boulder.sf, 660), playgrounds.sf))) %>% 
-  mutate(boulder.sf, school.Buffer=lengths(st_intersects(st_buffer(boulder.sf, 660), schoolPoints.sf)))
-
-boulder.sf <-
-  st_join(boulder.sf, parcels.sf) 
-boulder.sf <-
-  st_join(boulder.sf, tracts)
+  mutate(boulder.sf, school.Buffer=lengths(st_intersects(st_buffer(boulder.sf, 660), schools.sf))) %>% 
+  mutate(boulder.sf, parks.Buffer=lengths(st_intersects(st_buffer(boulder.sf, 660), parks.sf)))
 
 st_drop_geometry(boulder.sf) %>% 
   dplyr::select(price, TotalFinishedSF, Age, playground.Buffer, SHAPEarea, school.Buffer) %>%
@@ -103,14 +115,29 @@ st_drop_geometry(boulder.sf) %>%
   labs(title = "Price as a function of continuous variables") +
   plotTheme()
 
+numericVars <- 
+  select_if(st_drop_geometry(boulder.sf), is.numeric) %>% 
+  dplyr::select(-MUSA_ID, -toPredict, -builtYear, -Stories, -UnitCount) %>% 
+  na.omit() 
+
+# only for continuous variables
+ggcorrplot(  #correlation plot
+  round(cor(numericVars), 1), 
+  p.mat = cor_pmat(numericVars),
+  show.diag = TRUE,
+  colors = c("#25CB10", "white", "#FA7800"),
+  type="lower",
+  insig = "blank") +  
+  labs(title = "Correlation across numeric variables") 
+
 reg1 <- lm(price ~ ., data = st_drop_geometry(boulder.sf) %>% 
              dplyr::select(price, Age, TotalFinishedSF, designCodeDscr, 
-                           bsmtType, qualityCode, nbrRoomsNobath, nbrFullBaths, 
-                           mainfloorSF, AcDscr, playground.Buffer, NAME.y))
+                           bsmtType, qualityCodeDscr, nbrRoomsNobath, nbrFullBaths, 
+                           mainfloorSF, AcDscr, playground.Buffer, NAME.y,
+                           parks.Buffer))
 
 summary(reg1)
 plot_summs(reg1)
-
 
 inTrain <- createDataPartition(
   y = paste(boulder.sf$NAME.y, boulder.sf$bsmtType, 
